885a4a605902176486fc502e48e42938cfdc7b45
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-04-13 11:07:19 +0300

    testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import './css/styles.css';
+// import fetchCountries from './fetchCountries.js';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 
-// GO in IT
+const refs = {
+  input: document.getElementById('search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+  console.log(event.target.value);
+  fetchCountries(event.target.value);
+}
+
+function fetchCountries(name) {
+  fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      createListMarkup(data);
+    });
+}
+
+function createListMarkup(country) {
+  const createMarkup = country
+    .map(({ name, flags }) => {
+      return `<li><svg>
+  <use href="${flags.svg}"></use>
+</svg>${name.common}</li>`;
+    })
+    .join('');
+
+  refs.list.innerHTML = createMarkup;
+}
